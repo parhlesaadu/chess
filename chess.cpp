eb4ff9a9c3e88f513f5e3b9fa1f2e0f74bf1ecc5
Saad Ahmed // سعد احمد <i221084@nu.edu.pk>
2024-10-21 21:56:16 +0500

    Update chess.cpp

--- chess.cpp
+++ chess.cpp
@@ -82,8 +82,8 @@ void YourNextMove(char chessboard[][8], int player) {
 	cout << "Enter the current column of the piece you wish to move: "; cin >> C;
 	while (R < '0' || R > '7' || C < '0' || C > '7' || chessboard[R - 48][C - 48] != piece) {
 		cout << "INVALID POSITION!\n";
-		cout << "Kindly reenter row: "; cin >> R;
-		cout << "Kindly reenter column: "; cin >> C;
+		cout << "Kindly reenter current row of the piece you wish to move: "; cin >> R;
+		cout << "Kindly reenter current column of the piece you wish to move: "; cin >> C;
 	}
 
 	int move, validOptions[45], index = 0, * movePTR = &move, r = R - 48, c = C - 48;
